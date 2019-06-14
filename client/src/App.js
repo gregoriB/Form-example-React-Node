c4cf0af875f272fab5380f81c21146e6683766b7
gregoriB <brandon.gregori@gmail.com>
2019-06-14 12:08:45 -0700

    react form example using node

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080", {
+      method: 'POST',
+      body: JSON.stringify({ "first": firstName, "last": lastName }),
+      headers: { 'Content-Type': 'applications/json' }
+    });
+    const json = await response.json();
+    setMessage(json);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='first name' value={firstName} onChange={e => setFirstName(e.target.value)}/>
+        <input type='text' placeholder='last name' value={lastName} onChange={e => setLastName(e.target.value)}/>
+        <button>SUBMIT</button>
+      </form>
+      <h1 className='display'>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'client/public/index.html')));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+app.use(express.json({ type: 'applications/json' }));
+
+app.post('/', (req, res) => {
+  const message = `Hello ${req.body.first} ${req.body.last}!`;
+  res.json(message);
+});
+
+const listener = app.listen(8080, () => console.log(`app listening on port: ${listener.address().port}`));
